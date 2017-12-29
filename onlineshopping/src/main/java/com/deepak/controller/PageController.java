25a21c956954e07115696f8bae859aba8ff0b43a
Deepak sahu <deepaksahu9119@gmail.com>
2017-12-29 21:33:19 +0530

    successfully commited

---
+++ onlineshopping/src/main/java/com/deepak/controller/PageController.java
@@ -0,0 +1,36 @@
+package com.deepak.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Welcome to Spring web MVC");
+		return mv;
+	}
+
+/*	@RequestMapping(value = "/test") // key=value
+	public ModelAndView test(@RequestParam(value = "greeting", required = false) String greeting) {
+		if (greeting == null) {
+			greeting = "Hello there";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}*/
+	
+	@RequestMapping(value = "/test/{greeting}") // key=value
+	public ModelAndView test(@PathVariable("greeting")String greeting ) {
+		if (greeting == null) {
+			greeting = "Hello there";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}
+}
